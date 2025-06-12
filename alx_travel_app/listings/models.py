1daf88f7de20042eab640924f1c043c659a7f452
p-prime1 <preciousnwaka95@gmail.com>
2025-06-12 16:30:06 +0100

    Defined database models, created serializers for API data representation adn implemented a management command to seed the database

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Listing(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField()
+    location = models.CharField(max_length=200)
+    listing_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    is_available = models.BooleanField()
+
+    def __str__(self):
+        return self.listing_id
+
+
+class Booking(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    booking_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    guest_name = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+
+
+class Review(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=CASCADE)
+    guest_name = models.CharField(max_length=200)
+    rating = models.TextField()
+    comment = models.TextField()
---
+++ alx_travel_app/listings/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Booking, Listing, Review
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
